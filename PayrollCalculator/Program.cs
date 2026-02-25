041d387ec2986a21d234f07043472e97a6cae2ba
ishan08526 <96050213+ishan08526@users.noreply.github.com>
2026-02-25 15:36:53 +1030

    Task1/2

--- PayrollCalculator/Program.cs
+++ PayrollCalculator/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-class Program
+﻿class Program
 {
     const double Tax_Rate = 0.2;
     static double CalculatePay(double hours, double rate)
@@ -21,27 +19,14 @@ class Program
             Console.Write("Enter Employee Name: ");
             string name = Console.ReadLine();
 
-            string [] parts = name.Split(' ');
-            if(parts.Length >= 2)
-            {
-                string first = parts[0];
-                string last = parts[1];
-
-                Console.Write("Enter Age: ");
-                string ageText = Console.ReadLine();
-                int age = int.Parse(ageText);
-
-                Person p = new Person(first, last, age);
-
-                Console.WriteLine("Full Name: " + p.FullName());
-                Console.WriteLine("Is Adult: " + p.isAdult());
-            }
-
             Console.Write("Hours Worked : ");
             string hoursText = Console.ReadLine();
 
             Console.Write("Hoursly Rate :");
             string rateText = Console.ReadLine();
+
+            Console.Write("Age : ");
+            string ageText = Console.ReadLine();
 
             double hours = double.Parse(hoursText);
             double rate = double.Parse(rateText);
@@ -52,17 +37,16 @@ class Program
         {
             Console.WriteLine("Error: "+ ex.Message);
         }
-
     }
 }
 
-public class Person
-{
-    public string FirstName{get; private set;}
-    public string LastName{get; private set;}
-    public int Age{get; private set;}
+    public class Person
+    {
+        public string FirstName{get; private set;}
+        public string LastName{get; private set;}
+        public int Age{get; private set;}
 
-    public Person(string firstName, string lastName, int age)
+        public Person(string firstName, string lastName, int age)
     {
         if (string.IsNullOrWhiteSpace(firstName))
             throw new ArgumentException("First name cannot be empty");
@@ -73,9 +57,9 @@ public class Person
         if (age<0)
             throw new ArgumentException("Age cannot be less than 0");
 
-        FirstName = firstName;
-        LastName  = lastName;
-        Age = age;
+            FirstName = firstName;
+            LastName  = lastName;
+            Age = age;
     }
     public string FullName()
     {
@@ -85,5 +69,5 @@ public class Person
     {
         return Age >= 18;
     }
-
 }
+
